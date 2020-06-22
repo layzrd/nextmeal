eaac41ea2c06e956cc18a086b1fde435b1649947
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-06-22 05:22:20 +0300

    added home page

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        "100": "100%",
+        "1/2": '50%',
+        "1/3": '33.333333%',
+        "1/4": '25%',
+        '1/6': '16.666667%',
+        "2/3": '66.666667%',
+        "3/4": '75%',
+        "100": "100%",
+        "1/2": '50%',
+        "1/3": '33.333333%',
+        "1/4": '25%',
+        "2/3": '66.666667%',
+        "3/4": '75%',
+      },
+      flex: {
+        'full-row': '0 1 100%',
+        'basis-100': '100%',
+        'basis-50': '50%',
+      },
+      height: {
+        'screen-40': '40vh',
+        'screen-60': '60vh',
+        'screen-85': '85vh',
+      },
+      gridTemplateRows: {
+        'ad-layout': 'minmax(3em, 5em) repeat(2, 1fr)',
+      },
+      colors: {
+        'prime': '#c45f4f',
+        'prime-200': '#f37866',
+        'prime-400': '#df6e5c',
+        'grayish': '#97a1a2',
+        'bluish': '#5f9cd2',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
